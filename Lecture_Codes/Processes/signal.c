933503fd61d2581909e35470227e561210b7c64d
Janons <candogrull@hotmail.com>
2025-12-08 12:44:26 +0100

    "Program to understand how signal handling works"

--- Lecture_Codes/Processes/signal.c
+++ Lecture_Codes/Processes/signal.c
@@ -1,1 +1,38 @@
 
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+//basic functions to handle a signal
+
+void handle(int sig, int timer){
+    printf("Recieved signal %d at time %d\n ", sig, timer);
+    exit(1);
+}
+
+
+int main(){
+    int i = 0;
+    while (1){
+        
+        //signal decleration
+        signal(SIGINT, handle);
+        sleep(2);
+
+        //increase the counter
+        i+= 1;
+        printf("Loop iteration: %d\n", i);
+
+        //the condition
+        if (i == 4){ 
+            signal(SIGTSTP,handle);
+            kill(getpid(), SIGTSTP);
+
+        }
+
+
+    }
+
+    return 0;
+}
